0d51b7c1553ffd671ea104323384259f69d9482b
macarena-chang <changmacarena@gmail.com>
2024-02-24 04:21:33 +0100

    Initial setup: Locations microservice

---
+++ locations-service/src/test/java/com/example/locationsservice/LocationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
