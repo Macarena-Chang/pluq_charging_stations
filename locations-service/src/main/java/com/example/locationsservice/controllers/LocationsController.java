89e73084d8bd6051ce64baadb8d1ea351fc15c81
macarena-chang <changmacarena@gmail.com>
2024-02-24 06:05:06 +0100

    Add get all locations, post location

--- locations-service/src/main/java/com/example/locationsservice/controllers/LocationsController.java
+++ locations-service/src/main/java/com/example/locationsservice/controllers/LocationsController.java
@@ -1,9 +1,31 @@
 package com.example.locationsservice.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.locationsservice.models.Location;
+import com.example.locationsservice.services.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/locations")
 public class LocationsController {
+    @Autowired
+    public final LocationService locationService;
+
+    @Autowired
+    public LocationsController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping
+    public List<Location> getAllLocations() {
+        return locationService.getAllLocations();
+    }
+
+    @PostMapping
+    public Location addLocation(@RequestBody Location location) {
+        return locationService.saveLocation(location);
+    }
+
 }
--- locations-service/src/main/java/com/example/locationsservice/models/Location.java
+++ locations-service/src/main/java/com/example/locationsservice/models/Location.java
@@ -3,6 +3,7 @@ package com.example.locationsservice.models;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
 
 @Data
 @Document(collection = "locations")
