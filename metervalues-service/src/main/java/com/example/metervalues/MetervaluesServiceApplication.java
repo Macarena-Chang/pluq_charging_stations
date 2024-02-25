63d2d0becfc60c799f1ce99dfd44e3ac8224863b
macarena-chang <changmacarena@gmail.com>
2024-02-25 16:33:10 +0100

    Initial setup: Meter Values microservice

---
+++ metervalues-service/src/main/java/com/example/metervalues/MetervaluesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.metervalues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetervaluesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetervaluesServiceApplication.class, args);
+	}
+
+}
