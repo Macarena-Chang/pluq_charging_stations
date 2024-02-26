dcdb1aca4c8b8da9e727d8db1aab15a4f115b349
macarena-chang <changmacarena@gmail.com>
2024-02-26 09:59:13 +0100

    Refactor

--- locations-service/src/main/java/com/example/locationsservice/controllers/LocationsController.java
+++ locations-service/src/main/java/com/example/locationsservice/controllers/LocationsController.java
@@ -3,6 +3,8 @@ package com.example.locationsservice.controllers;
 import com.example.locationsservice.entities.Location;
 import com.example.locationsservice.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,13 +21,17 @@ public class LocationsController {
     }
 
     @GetMapping
-    public List<Location> getAllLocations() {
-        return locationService.getAllLocations();
+    public ResponseEntity<List<Location>> getAllLocations() {
+        List<Location> locations = locationService.getAllLocations();
+        return ResponseEntity.ok(locations); //200
     }
 
     @PostMapping
-    public Location addLocation(@RequestBody Location location) {
-        return locationService.saveLocation(location);
+    public ResponseEntity<Location> addLocation(@RequestBody Location location) {
+        Location savedLocation = locationService.saveLocation(location);
+        return new ResponseEntity<>(savedLocation, HttpStatus.CREATED); //201
     }
+
+    //TODO validation location and exception handling. Dtos.
 
 }
