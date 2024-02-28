6e43a80e485956126c53e9698980420796106cc4
macarena-chang <changmacarena@gmail.com>
2024-02-28 11:35:19 +0100

    Add OpenAPI specification for Locations service (Swagger)

---
+++ locations-service/src/main/java/com/example/locationsservice/conf/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.locationsservice.conf;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Locations")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("PLUQ").version("1.0.0"));
+    }
+}
